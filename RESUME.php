0f2f99b5cbed9e26356a785f0cd2c030d3d932a0
MonoXIU <125653118+MonoXIU@users.noreply.github.com>
2024-04-17 02:08:43 +0800

    Add files via upload

---
+++ RESUME.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="WEIWEI, 履歷">
+<title>WEIWEI履歷表</title>
+</head>
+
+<center> <font size ="30", color = "black"><b>履歷表</b></font></center>
+<hr>
+<h2>自我介紹</h2>
+<ul>
+    <li><b>姓名</b>：尉修華</li>
+    <li><b>學校</b>：高雄大學</li>
+    <li><b>科系</b>：資管</li>
+    <li><b>興趣</b>：看小說，寫小說</li>
+    <li><b>個性</b>：冷靜</li>
+</ui>    
+<br/>
+<hr>
+<h2>個人資料</h2>
+<h3>
+<p>
+手機號碼：0968XXX668<br/>E-mail：x@gnail.com<br/>戶籍地址：高市C區<br/>通訊地址：同上<br/>
+</p> 
+</h3>
+<hr>
+
+<h2>工作經歷</h2>
+
+<table border="">
+    <caption>工作過往</caption>
+    <thread>
+        <tr>
+            <th>地點</th>
+            <th>工時</th>
+            <th>業務</th>
+        </tr>
+    </thread>
+    <tbody>
+        <tr>
+            <td>小七</td>
+            <td>2023年1月~2022年4月</td>
+            <td>收銀</td>
+        </tr>
+        <tr>
+            <td>家教</td>
+            <td>2023年5月~2024年2月</td>
+            <td>教書</td>
+        </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
